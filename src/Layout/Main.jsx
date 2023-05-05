1a870e079964748cc8b6ef887c1b0022eaafcb57
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-05 07:09:17 +0600

    footer degin complete

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../component/Header/Header';
+import Footer from '../component/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
