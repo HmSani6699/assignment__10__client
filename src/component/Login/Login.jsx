c2eec1610548294536e81d615e976c4e330bb439
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-05 10:55:55 +0600

    added to register togol button

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content">
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
-                    </div>
-                    <div className="form-control mt-6">
-                                <Link to='/login'>Already hauv an acount ? Please <button className="btn btn-link">Login</button></Link>
-                            </div>
-                </form>
+            <div className="hero-content">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <div className="form-control mt-6">
+                            <Link to='/register'>Creat  a new  acount ? Please<button className="btn btn-link">Register</button></Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
